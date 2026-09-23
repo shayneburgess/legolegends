1805111e9693cc60dc40a87f072aa389b529eff0
Rob McDonald <robmcd@gmail.com>
2026-09-22 20:21:17 -0700

    updating run 7

--- 2026/missions/run_1.py
+++ 2026/missions/run_1.py
@@ -1,4 +1,4 @@
-# LEGO slot:1
+# LEGO slot:1 autostart
 
 from drive_tools import *
 
@@ -6,10 +6,11 @@ mission = Robot()
 
 
 async def main():
-	await mission.drive(80, speed=50)
+	print("RUN 1 REV 2026-09-22")
+	await mission.drive(60, speed=50)
 	await mission.turn(-100)
 	await mission.turn(100)
-	await mission.drive(-80, speed=50)
+	await mission.drive(-60, speed=50)
 	#await mission.turn(-45,100)
 	#await mission.drive(10)
 	#await mission.turn(-45)
