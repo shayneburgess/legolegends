72d0099ceacb74ec44ec5e7837b58fe203564954
Jackson Burgess <jack@gmail.com>
2026-09-22 20:33:12 -0700

    Test 7

--- 2026/missions/run_7.py
+++ 2026/missions/run_7.py
@@ -1,4 +1,4 @@
-# LEGO slot:7
+# LEGO slot:7 autostart
 
 from drive_tools import *
 
@@ -6,9 +6,8 @@ mission = Robot()
 
 
 async def main():
-	await mission.drive(20)
-	await mission.turn(-15)
-	await mission.drive(37)
-	await mission.turn(90)
+	await mission.drive(40)
+	await mission.drive(-40)
+
 
 runloop.run(main())
