75a7aa075d80eb2928f11d6859982a47cc1ca8b7
Jackson Burgess <jack@gmail.com>
2026-09-22 20:41:22 -0700

    fix 7

--- 2026/missions/run_7.py
+++ 2026/missions/run_7.py
@@ -6,8 +6,9 @@ mission = Robot()
 
 
 async def main():
-	await mission.drive(40)
-	await mission.drive(-40)
+	await mission.drive(20)
+	await mission.turn(-15)
+	await mission.drive(37)
 
 
 runloop.run(main())
